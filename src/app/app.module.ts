d5b9e232b2ba818cd20112c4f027cf095a4d2bea
HarikaDubba <80460010+HarikaDubba@users.noreply.github.com>
2021-04-19 15:42:34 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,45 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './components/shared/nav/nav.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { CartComponent } from './components/shopping-cart/cart/cart.component';
-import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
-import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
-import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
-import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {Ng2SearchPipeModule} from 'ng2-search-filter';
+import { HighlightDirective } from './_directives/highlight.directive';
+import { AdminComponent } from './admin/admin.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { EditProductComponent } from './admin/edit-product/edit-product.component';
+ 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    HeaderComponent,
-    FooterComponent,
-    ShoppingCartComponent,
-    CartComponent,
-    FiltersComponent,
+    NavbarComponent,
     ProductListComponent,
-    ProductItemComponent,
-    CartItemComponent
+    ProductDetailsComponent,
+    CartComponent,
+    HighlightDirective,
+    AdminComponent,
+    ProductsComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    IvyCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
